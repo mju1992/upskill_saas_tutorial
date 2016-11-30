831b829b68edcb735fee247c3d1726fd293ba056
Matthew Unthank <matthew.unthank@gmail.com>
2016-11-30 07:40:45 +0000

    Edit environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,13 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMPT_PORT']
-  :address        => ENV['MAILGUN_SMPT_SERVER']
-  :user_name      => ENV['MAILGUN_SMPT_logini']
-  :password       => ENV['MAILGUN_SMPT_PASSWORD']
+  :port           => ENV['MAILGUN_SMPT_PORT'],
+  :address        => ENV['MAILGUN_SMPT_SERVER'],
+  :user_name      => ENV['MAILGUN_SMPT_LOGIN'],
+  :password       => ENV['MAILGUN_SMPT_PASSWORD'],
   :domain         => 'murmuring-garden-87930.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smpt
+
+
