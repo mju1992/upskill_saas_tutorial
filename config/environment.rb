b5e22493f9e46407e2e8c6fd24a1df76aceebf1a
Matthew Unthank <matthew.unthank@gmail.com>
2016-11-30 07:09:30 +0000

    Added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMPT_PORT']
+  :address        => ENV['MAILGUN_SMPT_SERVER']
+  :user_name      => ENV['MAILGUN_SMPT_logini']
+  :password       => ENV['MAILGUN_SMPT_PASSWORD']
+  :domain         => 'murmuring-garden-87930.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smpt
