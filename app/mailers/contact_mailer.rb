38e46f95f06d459e867b6cf2050678589ae25584
Matthew Unthank <matthew.unthank@gmail.com>
2016-11-30 07:34:08 +0000

    .

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,10 +2,11 @@ class ContactMailer < ActionMailer::Base
     default to: 'matthew.unthank@gmail.com'
     
     def contact_email(name, email, body)
-      @name = name
-      @email = email 
-      @body = body
+     @name = name
+     @email = email 
+     @body = body
       
-      mail(from: email, subject: 'Contact Form Message')
+      
+     mail(from: email, subject: 'Contact Form Message')
     end
 end
